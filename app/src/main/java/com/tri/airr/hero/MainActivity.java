5828a6cfc8d50ba8d63bd197d65fcbff8b95d122
drossos <drossos@bayviewglen.ca>
2017-07-31 15:28:49 -0400

    first bluetooth connection attempt

--- app/src/main/java/com/tri/airr/hero/MainActivity.java
+++ app/src/main/java/com/tri/airr/hero/MainActivity.java
@@ -63,14 +63,15 @@ public class MainActivity extends AppCompatActivity {
             else if (!bluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, 0);
-            }
-
-            if (bluetoothAdapter.isEnabled()){
-                Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
-                    if (!list()){
+                 if (bluetoothAdapter.isEnabled()){
+                     Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+                        if (!list()){
                         Toast.makeText(getApplicationContext(), "HERO is not a paired device. Please pair hero first",Toast.LENGTH_LONG).show();
                     }
                 }
+            }
+
+
 
 
         }
